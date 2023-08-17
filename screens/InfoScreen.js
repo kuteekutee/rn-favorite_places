fc0ff714d3c5bea559e035ea7b46552ea703c1ad
kuteekutee <bernardsng@gmail.com>
2023-08-17 18:26:26 +0800

    Added scrollview in InfoScreen

--- screens/InfoScreen.js
+++ screens/InfoScreen.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { Colors } from "../constants/colors";
 import { populatePlace } from "../util/database";
 import { Ionicons } from "@expo/vector-icons";
@@ -11,73 +18,77 @@ function InfoScreen({ navigation }) {
   }
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.paragraphContainer}>
-        <Text style={styles.paragraphHeader}>In a nutshell ... </Text>
-        <Text style={styles.paragraph}>
-          A simple RN mobile app that can capture and save all your favorite
-          places. All that is requires is tht you enable the use of your phone
-          Location and allow permissions to use its Camera.
-        </Text>
-      </View>
-      <View style={styles.paragraphContainer}>
-        <Text style={styles.paragraphHeader}>Usage</Text>
-        <Text style={styles.paragraph}>
-          · Tab on "My Place" from the bottom navigation bar
-        </Text>
-        <Text style={styles.paragraph}>
-          · Tab on{" "}
-          <Ionicons
-            name="add-circle-outline"
-            size={30}
-            color={Colors.primary100}
-          />{" "}
-          at the upper right hand corner on the screen
-        </Text>
-        <Text style={styles.paragraph}>
-          · In "Add A Place Favorite" screen provide a placw title or short
-          description
-        </Text>
-        <Text style={styles.paragraph}>
-          · Tab "Take Image" to take a picture
-        </Text>
-        <Text style={styles.paragraph}>
-          · For the location, either Tab on "Locate User" or "Pick On Map"
-        </Text>
-        <Text style={styles.paragraph}>
-          · Note that only after having completed all the inputs will a "Save
-          Place" button be shown!
-        </Text>
-        <Text style={styles.paragraph}>
-          · Tab on "Save Place" and your entry will be added to the top on the
-          list in "My Places" screen.
-        </Text>
-      </View>
-      <View style={styles.resetContainer}>
+      <ScrollView>
         <View style={styles.paragraphContainer}>
-          <Text style={styles.paragraphHeader}>Reset Application</Text>
+          <Text style={styles.paragraphHeader}>In a nutshell ... </Text>
           <Text style={styles.paragraph}>
-            Clear all favorite places and repopulate sample data for demo
-            purposes.
+            A simple RN mobile app that can capture and save all your favorite
+            places. All that is requires is tht you enable the use of your phone
+            Location and allow permissions to use its Camera.
           </Text>
         </View>
-        <Pressable
-          // onPress={() => {}}
-          onPress={resetPlacesHandler}
-          style={({ pressed }) => [
-            {
-              backgroundColor: pressed ? Colors.primary100 : Colors.primary200,
-            },
-            styles.wrapperCustom,
-          ]}
-        >
-          {({ pressed }) => (
-            <Text style={styles.resetText}>
-              {pressed ? "Reset Again" : "Reset Places"}
+        <View style={styles.paragraphContainer}>
+          <Text style={styles.paragraphHeader}>Usage</Text>
+          <Text style={styles.paragraph}>
+            · Tab on "My Place" from the bottom navigation bar
+          </Text>
+          <Text style={styles.paragraph}>
+            · Tab on{" "}
+            <Ionicons
+              name="add-circle-outline"
+              size={30}
+              color={Colors.primary100}
+            />{" "}
+            at the upper right hand corner on the screen
+          </Text>
+          <Text style={styles.paragraph}>
+            · In "Add A Place Favorite" screen provide a placw title or short
+            description
+          </Text>
+          <Text style={styles.paragraph}>
+            · Tab "Take Image" to take a picture
+          </Text>
+          <Text style={styles.paragraph}>
+            · For the location, either Tab on "Locate User" or "Pick On Map"
+          </Text>
+          <Text style={styles.paragraph}>
+            · Note that only after having completed all the inputs will a "Save
+            Place" button be shown!
+          </Text>
+          <Text style={styles.paragraph}>
+            · Tab on "Save Place" and your entry will be added to the top on the
+            list in "My Places" screen.
+          </Text>
+        </View>
+        <View style={styles.resetContainer}>
+          <View style={styles.paragraphContainer}>
+            <Text style={styles.paragraphHeader}>Reset Application</Text>
+            <Text style={styles.paragraph}>
+              Clear all favorite places and repopulate sample data for demo
+              purposes.
             </Text>
-          )}
-        </Pressable>
-        <Text style={styles.warningText}>!!! ALL DATA WILL BE LOST !!!</Text>
-      </View>
+          </View>
+          <Pressable
+            // onPress={() => {}}
+            onPress={resetPlacesHandler}
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed
+                  ? Colors.primary100
+                  : Colors.primary200,
+              },
+              styles.wrapperCustom,
+            ]}
+          >
+            {({ pressed }) => (
+              <Text style={styles.resetText}>
+                {pressed ? "Reset Again" : "Reset Places"}
+              </Text>
+            )}
+          </Pressable>
+          <Text style={styles.warningText}>!!! ALL DATA WILL BE LOST !!!</Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
